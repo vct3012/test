2d7b5bb6f6c5f176e046f8d82338aaddd4a05d36
tan <vct3012.git@gmail.com>
2024-12-18 16:43:33 +0700

    test lan 2

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import random from "random";
 const path = "./data.json";
 
 const isValidDate = (date) => {
-  const startDate = moment("2019-01-01");
-  const endDate = moment("2024-12-13");
+  const startDate = moment("2024-01-01");
+  const endDate = moment("2024-12-17");
   return date.isBetween(startDate, endDate, null, "[]");
 };
 
@@ -27,7 +27,7 @@ const makeCommits = async (n) => {
     const randomWeeks = random.int(0, 54 * 4);
     const randomDays = random.int(0, 6);
 
-    const randomDate = moment("2019-01-01")
+    const randomDate = moment("2022-01-01")
       .add(randomWeeks, "weeks")
       .add(randomDays, "days");
 
